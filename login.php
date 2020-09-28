9786a74e746538fc174268d46980f04453429d3b
Parsa Yazdani <3parsa3@gmail.com>
2020-09-28 22:47:19 +1000

    Update login.php

--- login.php
+++ login.php
@@ -1,9 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
     <meta name="author" content="Parsa Yazdani (Quix)">
     <title>Droppler</title>
     <link href="https://getbootstrap.com/docs/4.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
@@ -26,19 +24,15 @@
     </style>
   </head>
   <body class="text-center">
-    <form class="form-signin">
+    <form class="form-signin" action="api/login" method="POST">
       <img class="mb-4" src="/assets/images/logo.png" alt="Logo" width="72" height="72">
       <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
       <label for="inputEmail" class="sr-only">Email address</label>
       <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
       <label for="inputPassword" class="sr-only">Password</label>
       <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
-      <div class="checkbox mb-3">
-        <label>
-          <input type="checkbox" value="remember-me"> Remember me
-        </label>
-      </div>
       <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+      <a href="register">Don't have an account? Register</a>
       <p class="mt-5 mb-3 text-muted">&copy; 2020-<?php echo date('Y');?></p>
     </form>
   </body>
